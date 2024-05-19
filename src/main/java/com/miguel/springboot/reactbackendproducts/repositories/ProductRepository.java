f1800a4144d9e86bbbac26c9d85a02540d9e3bab
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-18 22:07:47 -0400

    RepositoryRestResource

--- src/main/java/com/miguel/springboot/reactbackendproducts/repositories/ProductRepository.java
+++ src/main/java/com/miguel/springboot/reactbackendproducts/repositories/ProductRepository.java
@@ -2,10 +2,12 @@ package com.miguel.springboot.reactbackendproducts.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.miguel.springboot.reactbackendproducts.entities.Product;
 
 /* Implementación de crud automático */
+@CrossOrigin(origins = "http://localhost:5173/")
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long>{
     
